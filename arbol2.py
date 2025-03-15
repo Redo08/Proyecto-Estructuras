899240f2370ddab1d29466fd7e072d07266595ab
Redo08 <juandavidreyesortiz8@gmail.com>
2025-03-15 18:48:22 -0500

    Permutacion

--- arbol2.py
+++ arbol2.py
@@ -1,3 +1,5 @@
+import math
+import copy
 class Nodo:
     def __init__(self, x, y, altura):
         self.valor=[x,y]
@@ -23,8 +25,32 @@ class Arbol:
                         self.insertar(raiz.derecha,x,y,altura+1)
                     else:
                         raiz.derecha=Nodo(x,y,altura)
-                else 
+                else:
+                    pass
                     
+           
+# Esto ya es main
+
+def es_valido_permutar(s): #Es valido
+    if len(set(s)) == len(s): #Si no estan repetidos
+        return True
     
-            
+def es_viable_permutar(s, n):
+    return len(s) <= n
+
+def permutar(nums, n,  s, solucion): #nums: Array inicial, n: Tamaño array, s: Array vacio para cada solución individual, solucion: Array para las soluciones totales
+    if es_valido_permutar(s):
+        if es_viable_permutar(s, n):
+            if n == len(s):
+                solucion.append(copy.deepcopy(s)) #Se necesita copiarlo asi, porque si no, se guarda es la referencia
+                if math.factorial(n) == len(solucion):
+                    print(solucion)
+                    print("El tamaño es: ", len(solucion))
+                else:
+                    for i in range(1, n+1):
+                        s.append(i)
+                        permutar(nums, n, s, solucion)
+                        s.pop()
+
+                
             
