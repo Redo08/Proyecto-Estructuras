0c90522dba1402dd6095e7fa5180a35eeb837dba
Redo08 <juandavidreyesortiz8@gmail.com>
2025-03-18 21:47:04 -0500

    Implementación de la permutación aplicada en los arboles, con grafica unica

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from src.arbol import Arbol
-from views.grafica import visualizar_arbol
+from src.arbol import Arbol, permutar #Traemos lo hecho en arbol, La clase Arbol y la función permutar
+from views.grafica import visualizar_arbol #Traemos la forma de visualizar los arboles
+
+#Ingreso de elementos del array
+array = [(5,8), (1,13), (10,15), (20,12), (11,15), (14,8)] #Entre archivo JSON
+arboles_individuales = set([]) #Eso es un conjunto para evitar repetidos
+contador = 0 #Contador para saber la cantidad de arboles unicos
+solucion = [] #Array solución
+
+#Llamado a permutar el array ingresado
+permutar(array, [], solucion)
+
 
 # Crear e insertar elementos en el árbol
-arbol = Arbol()
-arbol.insertar(5, 8)
-arbol.insertar(1, 13)
-arbol.insertar(10, 15)
-arbol.insertar(20, 12)
-arbol.insertar(11, 15)
-arbol.insertar(14, 8)
+for i in solucion:
+    arbol = Arbol() #Se tiene que crear un nuevo arbol para cada uno
+    tamaño = len(arboles_individuales) #Se saca el tamaño del conjunto antes de insertar el siguiente arbol
+    for j in i:
+        arbol.insertar(j[0], j[1])  #Se recorre la sublista y se sacan los valores x, y
+    arboles_individuales.add(( #Insertamos los recorridos inorden y preorden del arbol para el conjunto, asi verificamos todos los casos posibles sin repetición 
+        tuple(map(tuple, arbol.recorrido_inorden())),
+        tuple(map(tuple,(arbol.recorrido_preorden()))) 
+    ))
+    if tamaño < len(arboles_individuales): #Verificamos si al insertar el nuevo valor, si se aumenta el tamaño, por lo que es un arbol nuevo
+        print("Recorrido inorden:", arbol.recorrido_inorden())  #Mostramos recorridos  
+        print("Recorrido preorden:", arbol.recorrido_preorden()) 
+        # Visualizar el árbol con PyGame
+        visualizar_arbol(arbol) #Vizualisamos el arbol
+        contador +=1 #Y sumamos al contador para saber cuantos arboles en total salen (68)
 
-print("Recorrido inorden:", arbol.recorrido_inorden())
+print(contador)
 
-# Visualizar el árbol con PyGame
-visualizar_arbol(arbol)
