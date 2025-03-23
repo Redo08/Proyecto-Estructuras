820e3087b5e9858f704b337ae0a03eccdfb2cd20
Redo08 <juandavidreyesortiz8@gmail.com>
2025-03-22 22:36:02 -0500

    Implementación casi funcional de plano, con area

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ utils = Utils()
 #Ingreso de elementos del arra y
 array = [(5,8), (1,13), (10,15), (20,12), (11,15), (14,8)] #Entre archivo JSON
 
+
 arboles = utils.permutaciones_unique_arbol(array)
 puntos = utils.permutaciones_unique_puntos(array)
 arbol = utils.generar_arbol(array)
@@ -17,9 +18,30 @@ arbol = utils.generar_arbol(array)
 print(arbol.recorrido_anchura_con_orientacion())
 recorrido_correcto = arbol.recorrido_anchura_con_orientacion()
 
-plano=Plano(recorrido_correcto)
-coordenadas_lineas = plano.generar_lineas()
-print(coordenadas_lineas)
+#plano=Plano(recorrido_correcto)
+#coordenadas_lineas, lineas_verticales, lineas_horizontales = plano.generar_lineas()
+# print(coordenadas_lineas)
+# print("----------------------------------------")
+# print("Lineas verticales", lineas_verticales)
+# print("----------------------------------------")
+# print("Lineas horizontales", lineas_horizontales)
+# print("Puntos intersección!")
+# print(plano.puntos_areas(lineas_verticales, lineas_horizontales))
+# print(len(plano.puntos_areas(lineas_verticales, lineas_horizontales)))
+
+arbol_optimo, area_optima = utils.areas_optimas(puntos)
+
+print(arbol_optimo.recorrido_anchura_con_orientacion())
+plano = Plano(arbol_optimo.recorrido_anchura_con_orientacion())
+lineas_verticales, lineas_horizontales = plano.generar_lineas()
+
+print(plano.lineas)
+
+# for i in coordenadas_lineas:
+#    for j in coordenadas_lineas:
+#       print(i[0])
+#       print(j[0])
+#       print(plano.distancia_entre_puntos(i[0], j[0]))
 
 
 #print(len(arboles))
@@ -30,7 +52,7 @@ print(coordenadas_lineas)
 #for i in permutaciones[0]:
    #grafica_arbol.visualizar_arbol(i)
 
-#grafica_arbol.visualizar_arbol(arboles[0])
+grafica_arbol.visualizar_arbol(arbol_optimo)
 
 
     
--- src/models/plano.py
+++ src/models/plano.py
@@ -1,6 +1,7 @@
 from  src.models.linea import Linea, Punto
 from  src.models.arbol import Arbol
-import matplotlib.pyplot as plt
+import copy
+import math
 
 class Plano:
     def __init__(self, lista_puntos):
@@ -8,18 +9,17 @@ class Plano:
         self.x_max = None
         self.y_min = None
         self.y_max = None
-        self.lineas = None #Es una lista que se calcula aqui
+        self.lineas = None #Es una lista que se calcula aqui ##########Lo ponemos ya como atributo?
         self.puntos = lista_puntos
         self.areas = None #Es una lista que se calcula aqui
         
     def calcular_limites(self): 
-        # puntos = [(2, 3, 0), (5, 7, 1), (8, 5, 0), (4, 6, 1),(7, 4, 1),(1,1,1),(3,3,1)]
-
     
         # El marco en realidad el minimo siempre seria 0
         marco_x = Punto([0, max(p[0] for p in self.puntos) + 1]) #(min, max) x
         marco_y = Punto([0, max(p[1] for p in self.puntos) + 1])# (min, max) y 
         
+        # Y el maximo siempre seria el maximo de los 2, x o y
         marco_superior = max(marco_x.posicion[1], marco_y.posicion[1])
         self.x_min = marco_x.posicion[0]
         self.x_max = marco_superior
@@ -35,6 +35,9 @@ class Plano:
         
         puntos = self.puntos #Sacamos el array de puntos
          
+        #Insertamos las lineas iniciales del plano
+        lineas_h.append((0, 0, self.x_max)) #Linea a lo largo del eje x
+        lineas_v.append((0, 0, self.y_max)) #Linea a lo largo del eje y
         #Definir limites de las rectas
         for i in puntos:
             x, y, alineacion = i[0], i[1], i[2] #Sacamos los valores de cada punto
@@ -72,20 +75,102 @@ class Plano:
                         if x > valor_x:
                             x_inicio = valor_x #Si esta a la derecha, se cambia el inicio
 
-                        #En el mismo lugar
-                        else:
-                            pass #No cambia nada y se mantienen los limites
-                lineas_h.append((y, x_inicio, x_final))
-                lineas_totales.append(((x,y),(y, x_inicio), (y, x_final)))
+                lineas_h.append((y, x_inicio, x_final)) 
+                lineas_totales.append(((x,y),(x_inicio, y), (x_final, y)))
+        
+        #Agregamos al final las lineas que cierran el plano
+        lineas_h.append((self.y_max, 0, self.x_max)) #Linea a lo largo del tope del plano
+        lineas_v.append((self.x_max, 0, self.y_max)) # Linea a largo del final del plano
+        lineas_h.sort() #Las ordenamos de menor a mayor
+        lineas_v.sort() #Las ordenamos de menor a mayor
+        self.lineas = lineas_totales
+        return lineas_v, lineas_h
+    
+    def puntos_interseccion_lineas_areas(self, lineas_v, lineas_h):
+        esquinas = [] #Esquinas de cada area
+        esquinas_set = set([]) #Para evitar repetidos
+        #Sacamos 2 lineas verticales(Como la lista esta ordenada no hay problema)
+        
+        #Primera Linea vertical
+        for i in range(len(lineas_v)-1): #Hasta el penultimo para evitar salirnos de la lista
+            x1, y_inicio1, y_final1 = lineas_v[i] 
+
+            #Segunda Linea vertical
+            for k in range(i+1, len(lineas_v)):
+                x2, y_inicio2, y_final2 = lineas_v[k]
                 
-        return lineas_totales
+                #Sacamos 2 lineas horizontales
+                
+                #Primera linea horizontal
+                for j in range(len(lineas_h)-1):
+                    y1, x_inicio1, x_final1 = lineas_h[j]
+                    
+                    #Segunda linea horizontal
+                    for z in range(j+1,len(lineas_h)):
+                        y2, x_inicio2, x_final2 = lineas_h[z]
+
+                        #Si las horizontales estan  entre el rango de  las 2 verticales
+                        if (y_inicio1 <= y1 <= y_final1 and y_inicio1 <= y2 <= y_final1) and (y_inicio2 <= y1 <= y_final2 and y_inicio2 <= y2 <= y_final2):
+                           # Si las verticales estan dentro del rango de las horizontales
+                           if (x_inicio1 <= x1 <= x_final1 and x_inicio1 <= x2 <= x_final1) and (x_inicio2 <= x1 <= x_final2 and x_inicio2 <= x2 <= x_final2):
+                                puntos_area = [(x1, y2), (x2, y2), (x2,y1), (x1, y1)] #Puntos de las esquinas del area
+                                
+                                solucion = frozenset(puntos_area) #Metemos los puntos a un conjunto inmutable para evitar repetidos
+                                if solucion not in esquinas_set: #Si no esta repetido, lo agrega
+                                    esquinas.append(puntos_area)
+                                    esquinas_set.add(solucion)
+        return esquinas
+        
+        
+    def filtrar_areas(self, areas): #Areas seria el array de esquinas
+        areas_perfectas = [] #Solución a retornr
+        lista_areas = [(puntos, self.generar_areas(puntos)) for puntos in areas] # Creamos una lista que tiene como valores una tupla, siendo la primera los puntos de las esquinas, y de segundo valor el area correspondiente a ese espacio
+        lista_areas.sort(key=lambda x: x[1], reverse=True) # Organiza la lista de mayor a menor. Toma el segundo valor para organizar, y el reverse es el que hace que sea de mayor a menor
+            
+        #Recorremos la lista
+        for i in range(len(lista_areas)):
+            #Area 1, sacamos los limites
+            area_1 = lista_areas[i][0]
+            min_x_1 = min(area_1[0][0], area_1[1][0], area_1[2][0], area_1[3][0]) #La minima componente en x
+            max_x_1 = max(area_1[0][0], area_1[1][0], area_1[2][0], area_1[3][0]) #La maxima componente en x
+            min_y_1 = min(area_1[0][1], area_1[1][1], area_1[2][1], area_1[3][1]) #La minima componente en y
+            max_y_1 = max(area_1[0][1], area_1[1][1], area_1[2][1], area_1[3][1]) #La maxima componente en y
+            
+            #Creamos una variable booleana para despues hacer el filtrado
+            es_contenedor = False
+            
+            for j in range(i+1,len(lista_areas)): #Se hace asi para que no se compruebe con el mismo
+                
+                #Area 2, sacamos los limites
+                area_2 = lista_areas[j][0]
+                min_x_2 = min(area_2[0][0], area_2[1][0], area_2[2][0], area_2[3][0]) #La minima componente en x    
+                max_x_2 = max(area_2[0][0], area_2[1][0], area_2[2][0], area_2[3][0]) #La maxima componente en x
+                min_y_2 = min(area_2[0][1], area_2[1][1], area_2[2][1], area_2[3][1]) #La minima componente en y
+                max_y_2 = max(area_2[0][1], area_2[1][1], area_2[2][1], area_2[3][1]) #La maxima componente en y
+                
+                #Validación
+                if min_x_1 <= min_x_2 and max_x_2 <= max_x_1 and min_y_1 <= min_y_2 and  max_y_2 <= max_y_1: #Si el area uno tiene sus limites conteniendo el area 2
+                    es_contenedor = True #Se asigna como contenedor y se sale
+                    break
+                
+            if not es_contenedor: #Si no es contenedor, se agrega
+                areas_perfectas.append(copy.deepcopy(lista_areas[i]))    
+        return areas_perfectas 
     
-    def generar_areas(self):
-        pass
     
-    def areas_optimas(self, listas_permutaciones_unique): # Lista de arboles únicos 
-        pass # Retorna el arbol que genera menor cantidad de areas
+    def distancia_entre_puntos(self, punto_1, punto_2): #Recibe los 2 puntos
+        return math.sqrt((punto_1[0] - punto_2[0])**2 + (punto_1[1] - punto_2[1])**2) #Formula de distancia euclediana
+        
+    def generar_areas(self, puntos_area):
+        #Sacar puntos necesarios
+        largo = self.distancia_entre_puntos(puntos_area[0], puntos_area[1]) #Se saca el largo
+        ancho = self.distancia_entre_puntos(puntos_area[1], puntos_area[2]) #Se saca el ancho
+
+        return largo*ancho
     
-    def interseccion_lineas(self):
-        pass
+    def puntos_areas(self, lineas_v, lineas_h): #Función llamada por el usuario
+        esquinas_sin_filtro = self.puntos_interseccion_lineas_areas(lineas_v, lineas_h) #Saca todas las esquinas
+
+        return self.filtrar_areas(esquinas_sin_filtro) #Devuelve solo las que si son
+    
     
